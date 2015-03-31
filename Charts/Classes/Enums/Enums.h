f802cd1d87297b9a215b908d96ea2e058168e8d5
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2015-03-31 10:43:04 -0400

    move to objc enums

---
+++ Charts/Classes/Enums/Enums.h
@@ -0,0 +1,13 @@
+//
+//  Enums.h
+//  Charts
+//
+//  Created by Pierre-Marc Airoldi on 2015-03-31.
+//  Copyright (c) 2015 dcg. All rights reserved.
+//
+
+#ifndef Charts_Enums_h
+#define Charts_Enums_h
+
+
+#endif
