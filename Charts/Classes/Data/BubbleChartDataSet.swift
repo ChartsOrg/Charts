cdfa6c232318fb783aeaaa2e228e2355c651164b
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2015-04-23 10:09:02 -0400

    Add license info to files

--- Charts/Classes/Data/BubbleChartDataSet.swift
+++ Charts/Classes/Data/BubbleChartDataSet.swift
@@ -2,9 +2,19 @@
 //  BubbleChartDataSet.swift
 //  Charts
 //
-//  Created by Pierre-Marc Airoldi on 2015-04-07.
-//  Copyright (c) 2015 Pierre-Marc Airoldi. All rights reserved.
+//  Copyright 2015 Pierre-Marc Airoldi
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
 import Foundation
 import CoreGraphics;
--- ChartsDemo/Classes/Demos/BubbleChartViewController.m
+++ ChartsDemo/Classes/Demos/BubbleChartViewController.m
@@ -1,15 +1,20 @@
 //
-//  ScatterChartViewController.m
+//  BubbleChartViewController.m
 //  ChartsDemo
 //
-//  Created by Daniel Cohen Gindi on 17/3/15.
+//  Copyright 2015 Pierre-Marc Airoldi
 //
-//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
-//  A port of MPAndroidChart for iOS
-//  Licensed under Apache License 2.0
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
-//  https://github.com/danielgindi/ios-charts
+//  http://www.apache.org/licenses/LICENSE-2.0
 //
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
 #import "BubbleChartViewController.h"
 #import "ChartsDemo-Swift.h"
