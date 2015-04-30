263be29fc61f823b8d198ecc6c95775807de8742
Daniel Cohen Gindi <danielgindi@gmail.com>
2015-04-30 19:33:23 +0300

    Make that comment clearer

--- Charts/Classes/Charts/BubbleChartView.swift
+++ Charts/Classes/Charts/BubbleChartView.swift
@@ -2,9 +2,9 @@
 //  BubbleChartView.swift
 //  Charts
 //
-//  Bubble chart implementation: Copyright 2015 Pierre-Marc Airoldi
-//
-//  Licensed under Apache License 2.0
+//  Bubble chart implementation:
+//    Copyright 2015 Pierre-Marc Airoldi
+//    Licensed under Apache License 2.0
 //
 //  https://github.com/danielgindi/ios-charts
 //
