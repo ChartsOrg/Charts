46a2a505ba965aa34b696c8af13d53fdd0e6a66c
Daniel Cohen Gindi <danielgindi@gmail.com>
2015-04-30 14:03:14 +0300

    Shortened license comment in header

--- Charts/Classes/Data/BubbleChartDataSet.swift
+++ Charts/Classes/Data/BubbleChartDataSet.swift
@@ -2,19 +2,12 @@
 //  BubbleChartDataSet.swift
 //  Charts
 //
-//  Copyright 2015 Pierre-Marc Airoldi
+//  Bubble chart implementation: Copyright 2015 Pierre-Marc Airoldi
 //
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
+//  Licensed under Apache License 2.0
 //
-//  http://www.apache.org/licenses/LICENSE-2.0
+//  https://github.com/danielgindi/ios-charts
 //
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
 
 import Foundation
 import CoreGraphics;
