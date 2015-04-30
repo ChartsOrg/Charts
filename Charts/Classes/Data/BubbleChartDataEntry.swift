803d576c44045f415fc9d1159770b87c780e6e0c
Daniel Cohen Gindi <danielgindi@gmail.com>
2015-04-30 19:34:51 +0300

    Code styling

--- Charts/Classes/Data/BubbleChartDataEntry.swift
+++ Charts/Classes/Data/BubbleChartDataEntry.swift
@@ -16,14 +16,14 @@ public class BubbleChartDataEntry: ChartDataEntry
     /// size value
     public var size = Float(0.0)
     
-    public init(xIndex: Int, value:Float, size: Float)
+    public init(xIndex: Int, value: Float, size: Float)
     {
         super.init(value: value, xIndex: xIndex)
         
         self.size = size
     }
     
-    public init(xIndex: Int, value:Float, size: Float, data: AnyObject?)
+    public init(xIndex: Int, value: Float, size: Float, data: AnyObject?)
     {
         super.init(value: value, xIndex: xIndex, data: data)
       
