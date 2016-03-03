79a6a0bd7ca1c6793a2f104a201f734eebfac610
da-na <dagna.bieda@gmail.com>
2016-03-03 11:01:08 -0500

    Improved lowestVisibleXIndex so that the line chart does not ignore first x value and returns 0 for lowestVisibleXIndex instead of 1

--- Charts/Classes/Charts/BarLineChartViewBase.swift
+++ Charts/Classes/Charts/BarLineChartViewBase.swift
@@ -1921,7 +1921,7 @@ public class BarLineChartViewBase: ChartViewBase, BarLineScatterCandleBubbleChar
     {
         var pt = CGPoint(x: viewPortHandler.contentLeft, y: viewPortHandler.contentBottom)
         getTransformer(.Left).pixelToValue(&pt)
-        return (pt.x <= 0.0) ? 0 : Int(round(pt.x + 1.0))
+        return (pt.x <= 0.0) ? 0 : Int(round(pt.x))
     }
     
     /// - returns: the highest x-index (value on the x-axis) that is still visible on the chart.
