1d9d51ee35c8bf52d20ebface872c3df96c01cc3
Daniel Cohen Gindi <danielgindi@gmail.com>
2016-05-02 23:53:38 +0300

    Fix possible zero stride error (Closes #981)

--- Charts/Classes/Renderers/ChartYAxisRenderer.swift
+++ Charts/Classes/Renderers/ChartYAxisRenderer.swift
@@ -137,7 +137,7 @@ public class ChartYAxisRenderer: ChartAxisRendererBase
                 let last = interval == 0.0 ? 0.0 : ChartUtils.nextUp(floor(Double(yMax) / interval) * interval)
                 
                 var n = 0
-                if interval != 0.0
+                if interval != 0.0 && last != first
                 {
                     for _ in first.stride(through: last, by: interval)
                     {
