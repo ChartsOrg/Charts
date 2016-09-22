ea525f6e7ade4c48d3aed42bdc63b93d989b28a1
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2016-09-21 23:14:02 -0400

    fix issues with charts needing carthage
    This will show errors in xcode if something is wrong and will auto download dependencies if the project cant find frameworks in the carthage folder.

---
+++ scripts/build-dependencies.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+function build_dependencies {
+    
+    has_dependencies=$(ls Carthage/Build/**/*.framework 2> /dev/null | wc -l)
+
+    if [ $has_dependencies != 0 ]
+    then
+        echo "Depencies have already been built."
+    else
+        echo "Building dependencies..."
+        carthage bootstrap
+    fi
+}
+
+function alert_to_install_carthage {
+    echo "error: Carthage was not found! In order to build Charts you need to use Carthage to build it's dependencies. Carthage can be downloaded from https://github.com/Carthage/Carthage."
+    exit 1
+}
+
+if hash carthage 2>/dev/null 
+then
+    build_dependencies
+else
+    alert_to_install_carthage
+fi
