629c548c886ec486c278409054c3fed39810003a
Maciej Trybilo <maciektr@gmail.com>
2017-05-05 12:03:19 +0100

    Allow turning off drag in X and Y axis separately.

--- Source/Charts/Charts/BarLineChartViewBase.swift
+++ Source/Charts/Charts/BarLineChartViewBase.swift
@@ -29,6 +29,8 @@ open class BarLineChartViewBase: ChartViewBase, BarLineScatterCandleBubbleChartD
     fileprivate var _pinchZoomEnabled = false
     fileprivate var _doubleTapToZoomEnabled = true
     fileprivate var _dragEnabled = true
+    fileprivate var _dragYEnabled = true
+    fileprivate var _dragXEnabled = true
     
     fileprivate var _scaleXEnabled = true
     fileprivate var _scaleYEnabled = true
@@ -853,8 +855,11 @@ open class BarLineChartViewBase: ChartViewBase, BarLineScatterCandleBubbleChartD
     {
         if gestureRecognizer == _panGestureRecognizer
         {
+            let velocity = _panGestureRecognizer.velocity(in: self)
             if _data === nil || !_dragEnabled ||
-                (self.hasNoDragOffset && self.isFullyZoomedOut && !self.isHighlightPerDragEnabled)
+                (self.hasNoDragOffset && self.isFullyZoomedOut && !self.isHighlightPerDragEnabled) ||
+                (!_dragYEnabled && fabs(velocity.y) > fabs(velocity.x)) ||
+                (!_dragXEnabled && fabs(velocity.y) < fabs(velocity.x))
             {
                 return false
             }
@@ -1517,7 +1522,31 @@ open class BarLineChartViewBase: ChartViewBase, BarLineScatterCandleBubbleChartD
             if _dragEnabled != newValue
             {
                 _dragEnabled = newValue
+                _dragYEnabled = newValue
+                _dragXEnabled = newValue
             }
+        }
+    }
+    
+    open var dragYEnabled: Bool {
+        get
+        {
+            return _dragYEnabled
+        }
+        set
+        {
+            _dragYEnabled = newValue
+        }
+    }
+    
+    open var dragXEnabled: Bool {
+        get
+        {
+            return _dragXEnabled
+        }
+        set
+        {
+            _dragXEnabled = newValue
         }
     }
     
