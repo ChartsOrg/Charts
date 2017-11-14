b7a35b0bfc158b66e43b28e80ae5c3abd97d9543
Jacob Christie <j.christie@icloud.com>
2017-11-13 22:09:31 -0800

    removed unnecessary comments

--- Source/Charts/Renderers/ChartDataRendererBase.swift
+++ Source/Charts/Renderers/ChartDataRendererBase.swift
@@ -16,44 +16,21 @@ import CoreGraphics
 public protocol DataRenderer: Renderer
 {
     @objc var animator: Animator? { get set }
-    
-//    @objc public init(animator: Animator?, viewPortHandler: ViewPortHandler)
-//    {
-//        super.init(viewPortHandler: viewPortHandler)
-//
-//        self.animator = animator
-//    }
 
     func drawData(context: CGContext)
-//    {
-//        fatalError("drawData() cannot be called on DataRenderer")
-//    }
 
     func drawValues(context: CGContext)
-//    {
-//        fatalError("drawValues() cannot be called on DataRenderer")
-//    }
 
     func drawExtras(context: CGContext)
-//    {
-//        fatalError("drawExtras() cannot be called on DataRenderer")
-//    }
 
     /// Draws all highlight indicators for the values that are currently highlighted.
     ///
     /// - parameter indices: the highlighted values
     func drawHighlighted(context: CGContext, indices: [Highlight])
-//    {
-//        fatalError("drawHighlighted() cannot be called on DataRenderer")
-//    }
 
     /// An opportunity for initializing internal buffers used for rendering with a new size.
     /// Since this might do memory allocations, it should only be called if necessary.
-    func initBuffers() //{ }
+    func initBuffers()
     
     func isDrawingValuesAllowed(dataProvider: ChartDataProvider?) -> Bool
-//    {
-//        guard let data = dataProvider?.data else { return false }
-//        return data.entryCount < Int(CGFloat(dataProvider?.maxVisibleCount ?? 0) * viewPortHandler.scaleX)
-//    }
 }
