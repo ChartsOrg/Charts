0011c353bd58f0fa60a7b5929804ee99b72ef03f
Alex Leeds <alexleeds@hotmail.com>
2018-01-11 21:02:42 -0500

    Fixed X-Axis Labels Not Showing (#3154) (#3174)

--- Source/Charts/Formatters/IndexAxisValueFormatter.swift
+++ Source/Charts/Formatters/IndexAxisValueFormatter.swift
@@ -53,7 +53,7 @@ open class IndexAxisValueFormatter: NSObject, IAxisValueFormatter
                              axis: AxisBase?) -> String
     {
         let index = Int(value.rounded())
-        guard values.indices.contains(index), index != Int(value) else { return "" }
+        guard values.indices.contains(index), index == Int(value) else { return "" }
         return _values[index]
     }
 }
