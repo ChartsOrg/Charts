5da58c0feef5846a8513f3a266b62eb2633b94c1
Xuan Liu <liuxuan30@gmail.com>
2019-03-04 09:41:53 +0800

    fix indent after replacing if with guard

--- Source/Charts/Renderers/XAxisRenderer.swift
+++ Source/Charts/Renderers/XAxisRenderer.swift
@@ -390,55 +390,53 @@ open class XAxisRenderer: AxisRendererBase
     {
         
         let label = limitLine.label
-        guard limitLine.drawLabelEnabled, !label.isEmpty else {
-            return
+        guard limitLine.drawLabelEnabled, !label.isEmpty else { return }
+
+        let labelLineHeight = limitLine.valueFont.lineHeight
+
+        let xOffset: CGFloat = limitLine.lineWidth + limitLine.xOffset
+        let attributes: [NSAttributedString.Key : Any] = [
+            .font : limitLine.valueFont,
+            .foregroundColor : limitLine.valueTextColor
+        ]
+
+        let (point, align): (CGPoint, NSTextAlignment)
+        switch limitLine.labelPosition {
+        case .topRight:
+            point = CGPoint(
+                x: position.x + xOffset,
+                y: viewPortHandler.contentTop + yOffset
+            )
+            align = .left
+
+        case .bottomRight:
+            point = CGPoint(
+                x: position.x + xOffset,
+                y: viewPortHandler.contentBottom - labelLineHeight - yOffset
+            )
+            align = .left
+
+        case .topLeft:
+            point = CGPoint(
+                x: position.x - xOffset,
+                y: viewPortHandler.contentTop + yOffset
+            )
+            align = .right
+
+        case .bottomLeft:
+            point = CGPoint(
+                x: position.x - xOffset,
+                y: viewPortHandler.contentBottom - labelLineHeight - yOffset
+            )
+            align = .right
         }
 
-            let labelLineHeight = limitLine.valueFont.lineHeight
-            
-            let xOffset: CGFloat = limitLine.lineWidth + limitLine.xOffset
-            let attributes: [NSAttributedString.Key : Any] = [
-                .font : limitLine.valueFont,
-                .foregroundColor : limitLine.valueTextColor
-            ]
-
-            let (point, align): (CGPoint, NSTextAlignment)
-            switch limitLine.labelPosition {
-            case .topRight:
-                point = CGPoint(
-                    x: position.x + xOffset,
-                    y: viewPortHandler.contentTop + yOffset
-                )
-                align = .left
-
-            case .bottomRight:
-                point = CGPoint(
-                    x: position.x + xOffset,
-                    y: viewPortHandler.contentBottom - labelLineHeight - yOffset
-                )
-                align = .left
-
-            case .topLeft:
-                point = CGPoint(
-                    x: position.x - xOffset,
-                    y: viewPortHandler.contentTop + yOffset
-                )
-                align = .right
-
-            case .bottomLeft:
-                point = CGPoint(
-                    x: position.x - xOffset,
-                    y: viewPortHandler.contentBottom - labelLineHeight - yOffset
-                )
-                align = .right
-            }
-
-            ChartUtils.drawText(
-                context: context,
-                text: label,
-                point: point,
-                align: align,
-                attributes: attributes
-            )
+        ChartUtils.drawText(
+            context: context,
+            text: label,
+            point: point,
+            align: align,
+            attributes: attributes
+        )
     }
 }
