162d924e5f754bfaca80ac23630d9942e7ff4887
oussama <ayed.oussama@gmail.com>
2019-04-17 23:50:26 +0100

    custom class update

--- Source/Charts/Renderers/CustomBarChartRenderer.swift
+++ Source/Charts/Renderers/CustomBarChartRenderer.swift
@@ -242,21 +242,17 @@ public class CustomBarChartRenderer: BarChartRenderer {
                 // Corner Radius for the bar charts
                  if (j % 4 == 0) {
                     // bottom of the bar chart
-                    let bezierPath = NSBezierPath(rect: barRect, roundedCorners: [.bottomRight, .bottomLeft], cornerRadius: 5)
-                        bezierPath.fill()
+                    let bezierPath = UIBezierPath(roundedRect: barRect, byRoundingCorners: [.bottomRight, .bottomLeft], cornerRadii: CGSize(width: 5.0, height: 0.0))
+                    context.addPath(bezierPath.cgPath)
                 }else if (j % 4 == 3 ){
                     //middle of the bar chart
-                    let bezierPath = NSBezierPath(rect: barRect, roundedCorners: [.topLeft, .topRight], cornerRadius: 5)
-                    bezierPath.fill()
-                        //NSBezierPath(roundedRect: barRect, xRadius: [.topLeft, .topRight], yRadius: CGSize(width: 5.0, height: 0.0))
+                    let bezierPath = UIBezierPath(roundedRect: barRect, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 5.0, height: 0.0))
 
-                    //context.addPath(bezierPath.cgPath)
+                    context.addPath(bezierPath.cgPath)
                 }else  {
                     //top of the bar chart
-                    let bezierPath = NSBezierPath(rect: barRect, roundedCorners: [.bottomRight, .bottomLeft], cornerRadius: 5)
-                    bezierPath.fill()
-                        //NSBezierPath(roundedRect: barRect, xRadius: [.bottomRight, .bottomLeft], yRadius: CGSize(width: 0.0, height: 0.0))
-                   // context.addPath(bezierPath.cgPath)
+                    let bezierPath = UIBezierPath(roundedRect: barRect, byRoundingCorners: [.bottomRight, .bottomLeft], cornerRadii: CGSize(width: 0.0, height: 0.0))
+                    context.addPath(bezierPath.cgPath)
                 }
 
 
@@ -298,23 +294,17 @@ public class CustomBarChartRenderer: BarChartRenderer {
             // Corner Radius for the bar charts
             if (j % 4 == 0) {
                 // bottom of the bar chart
-                let bezierPath = NSBezierPath(rect: barRect, roundedCorners: [.bottomRight, .bottomLeft], cornerRadius: 5)
-                bezierPath.fill()
-                    //UIBezierPath(roundedRect: barRect, byRoundingCorners: [.bottomRight, .bottomLeft], cornerRadii: CGSize(width: 5.0, height: 0.0))
-               // context.addPath(bezierPath.cgPath)
+                let bezierPath = UIBezierPath(roundedRect: barRect, byRoundingCorners: [.bottomRight, .bottomLeft], cornerRadii: CGSize(width: 5.0, height: 0.0))
+                context.addPath(bezierPath.cgPath)
             }else if (j % 4 == 3 ){
                 //middle of the bar chart
-                let bezierPath = NSBezierPath(rect: barRect, roundedCorners: [.topLeft, .topRight], cornerRadius: 5)
-                bezierPath.fill()
-                    //UIBezierPath(roundedRect: barRect, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 5.0, height: 0.0))
+                let bezierPath = UIBezierPath(roundedRect: barRect, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 5.0, height: 0.0))
 
-             //   context.addPath(bezierPath.cgPath)
+                context.addPath(bezierPath.cgPath)
             }else{
                 //top of the bar chart
-                let bezierPath = NSBezierPath(rect: barRect, roundedCorners: [.bottomRight, .bottomLeft], cornerRadius: 5)
-                bezierPath.fill()
-                    //UIBezierPath(roundedRect: barRect, byRoundingCorners: [.bottomRight, .bottomLeft], cornerRadii: CGSize(width: 0.0, height: 0.0))
-              //  context.addPath(bezierPath.cgPath)
+                let bezierPath = UIBezierPath(roundedRect: barRect, byRoundingCorners: [.bottomRight, .bottomLeft], cornerRadii: CGSize(width: 0.0, height: 0.0))
+                context.addPath(bezierPath.cgPath)
             }
 
             context.drawPath(using: .fill)
@@ -670,10 +660,9 @@ public class CustomBarChartRenderer: BarChartRenderer {
                 setHighlightDrawPos(highlight: high, barRect: barRect)
                 context.setStrokeColor(set.highlightColor.cgColor)
 
-                let bezierPath = NSBezierPath(roundedRect: barRect, xRadius: 5, yRadius: 5)
-                    //UIBezierPath(roundedRect: barRect, cornerRadius: 5)
-                //context.addPath(bezierPath.cgPath)
-                bezierPath.fill()
+                let bezierPath = UIBezierPath(roundedRect: barRect, cornerRadius: 5)
+                context.addPath(bezierPath.cgPath)
+
                 context.drawPath(using: .stroke)
             }
         }
@@ -682,111 +671,4 @@ public class CustomBarChartRenderer: BarChartRenderer {
     }
    
     
-}
-
-public struct Corners: OptionSet {
-    public let rawValue: Int
-    
-    public init(rawValue: Corners.RawValue) {
-        self.rawValue = rawValue
-    }
-    
-    public static let topLeft = Corners(rawValue: 1 << 0)
-    public static let bottomLeft = Corners(rawValue: 1 << 1)
-    public static let topRight = Corners(rawValue: 1 << 2)
-    public static let bottomRight = Corners(rawValue: 1 << 3)
-    
-    public func flipped() -> Corners {
-        var flippedCorners: Corners = []
-        
-        if contains(.bottomRight) {
-            flippedCorners.insert(.topRight)
-        }
-        
-        if contains(.topRight) {
-            flippedCorners.insert(.bottomRight)
-        }
-        
-        if contains(.bottomLeft) {
-            flippedCorners.insert(.topLeft)
-        }
-        
-        if contains(.topLeft) {
-            flippedCorners.insert(.bottomLeft)
-        }
-        
-        return flippedCorners
-    }
-}
-
-
-public extension NSBezierPath {
-    
-    // Compatibility bewteen NSBezierPath and UIBezierPath
-    #if os(iOS) || os(tvOS)
-    public func curve(to point: CGPoint, controlPoint1: CGPoint, controlPoint2: CGPoint) {
-        addCurve(to: point, controlPoint1: controlPoint1, controlPoint2: controlPoint2)
-    }
-    
-    public func line(to point: CGPoint) {
-        addLine(to: point)
-    }
-    #endif
-    
-    public convenience init(rect: CGRect, roundedCorners: Corners, cornerRadius: CGFloat) {
-        self.init()
-        
-        // On iOS & tvOS, we need to flip the corners
-        #if os(iOS) || os(tvOS)
-        let corners = roundedCorners.flipped()
-        #elseif os(macOS)
-        let corners = roundedCorners
-        #endif
-        
-        let maxX: CGFloat = rect.size.width
-        let minX: CGFloat = 0
-        let maxY: CGFloat = rect.size.height
-        let minY: CGFloat =  0
-        
-        let bottomRightCorner = CGPoint(x: maxX, y: minY)
-        
-        
-        if corners.contains(.bottomRight) {
-            line(to: CGPoint(x: maxX - cornerRadius, y: minY))
-            curve(to: CGPoint(x: maxX, y: minY + cornerRadius), controlPoint1: bottomRightCorner, controlPoint2: bottomRightCorner)
-        }
-        else {
-            line(to: bottomRightCorner)
-        }
-        
-        let topRightCorner = CGPoint(x: maxX, y: maxY)
-        
-        if corners.contains(.topRight) {
-            line(to: CGPoint(x: maxX, y: maxY - cornerRadius))
-            curve(to: CGPoint(x: maxX - cornerRadius, y: maxY), controlPoint1: topRightCorner, controlPoint2: topRightCorner)
-        }
-        else {
-            line(to: topRightCorner)
-        }
-        
-        let topLeftCorner = CGPoint(x: minX, y: maxY)
-        
-        if corners.contains(.topLeft) {
-            line(to: CGPoint(x: minX + cornerRadius, y: maxY))
-            curve(to: CGPoint(x: minX, y: maxY - cornerRadius), controlPoint1: topLeftCorner, controlPoint2: topLeftCorner)
-        }
-        else {
-            line(to: topLeftCorner)
-        }
-        
-        let bottomLeftCorner = CGPoint(x: minX, y: minY)
-        
-        if corners.contains(.bottomLeft) {
-            line(to: CGPoint(x: minX, y: minY + cornerRadius))
-            curve(to: CGPoint(x: minX + cornerRadius, y: minY), controlPoint1: bottomLeftCorner, controlPoint2: bottomLeftCorner)
-        }
-        else {
-            line(to: bottomLeftCorner)
-        }
-    }
 }
