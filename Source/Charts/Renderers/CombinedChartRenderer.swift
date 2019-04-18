180a23a04749fdf6228969b7691b3adc2ece4404
oussama <ayed.oussama@gmail.com>
2019-04-18 10:15:03 +0100

    update Custom Class combined bar chart

--- Source/Charts/Renderers/CombinedChartRenderer.swift
+++ Source/Charts/Renderers/CombinedChartRenderer.swift
@@ -49,7 +49,8 @@ open class CombinedChartRenderer: DataRenderer
             case .bar:
                 if chart.barData !== nil
                 {
-                    _renderers.append(BarChartRenderer(dataProvider: chart, animator: animator, viewPortHandler: viewPortHandler))
+                    _renderers.append(CustomCombinedBarChartRenderer(dataProvider: chart, animator: animator, viewPortHandler: viewPortHandler))
+                    //_renderers.append(BarChartRenderer(dataProvider: chart, animator: animator, viewPortHandler: viewPortHandler))
                 }
                 break
                 
