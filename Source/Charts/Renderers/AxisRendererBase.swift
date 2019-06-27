a513e6fcfb3a90d47b493595d2e7b4924a7045d8
Paramasivan Samuthiram <siva@cosmicinsights.net>
2019-06-27 11:55:15 +0530

    Fixed a crash while zooming in the chart
    Change done -
    Checked for isNan on both on 1) AxisRendererBase.swift and YAxisRendererRadarChart.swift

--- Source/Charts/Renderers/AxisRendererBase.swift
+++ Source/Charts/Renderers/AxisRendererBase.swift
@@ -97,7 +97,7 @@ open class AxisRendererBase: Renderer
         let labelCount = axis.labelCount
         let range = abs(yMax - yMin)
         
-        if labelCount == 0 || range <= 0 || range.isInfinite
+        if labelCount == 0 || range <= 0 || range.isInfinite || range.isNaN
         {
             axis.entries = [Double]()
             axis.centeredEntries = [Double]()
