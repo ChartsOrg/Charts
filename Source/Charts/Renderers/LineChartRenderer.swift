afec120818d264c329567ab127560e3d8476900f
Ruchi Upgade <71132017+ruchiupgade-mb@users.noreply.github.com>
2022-03-10 18:14:06 +0530

    Update LineChartRenderer.swift

--- Source/Charts/Renderers/LineChartRenderer.swift
+++ Source/Charts/Renderers/LineChartRenderer.swift
@@ -891,7 +891,7 @@ open class LineChartRenderer: LineRadarRenderer
         // there is the possibility of some labels being rounded up. A floor() might fix this, but seems to be a brute force solution.
         let label = "\(Int(e.x))"
 
-        let elementValueText = dataSet.valueFormatter.stringForValue(e.y,
+        let elementValueText = dataSet.valueFormatter.stringForValue(e.y.rounded(.up),
                                                                      entry: e,
                                                                      dataSetIndex: dataSetIndex,
                                                                      viewPortHandler: viewPortHandler)
