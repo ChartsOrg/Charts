d768a74f9e5adee4bb2bc878a8f720af8e72e526
Felix Herrmann <felix.herrmann@icloud.com>
2022-05-26 22:09:59 +0200

    return last index for every rounding

--- Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
+++ Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
@@ -217,7 +217,7 @@ open class ChartDataSet: ChartBaseDataSet
         rounding: ChartDataSetRounding) -> Int
     {
         var closest = partitioningIndex { $0.x >= xValue }
-        guard closest < endIndex else { return rounding == .closest ? (endIndex-1) : -1 }
+        guard closest < endIndex else { return index(before: endIndex) }
 
         var closestXValue = self[closest].x
 
