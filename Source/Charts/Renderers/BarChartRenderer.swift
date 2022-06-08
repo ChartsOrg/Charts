03b6a88c0ca1154982970dae3e7cfd847b93aa4c
David Vizaknai <david@halcyonmobile.com>
2022-06-08 12:24:42 +0300

    Add rounded corners for bars

--- Source/Charts/Renderers/BarChartRenderer.swift
+++ Source/Charts/Renderers/BarChartRenderer.swift
@@ -378,8 +378,11 @@ open class BarChartRenderer: BarLineScatterCandleBubbleRenderer
                 // Set the color for the currently drawn value. If the index is out of bounds, reuse colors.
                 context.setFillColor(dataSet.color(atIndex: j).cgColor)
             }
-            
-            context.fill(barRect)
+
+            let bezierPath = UIBezierPath(roundedRect: barRect, cornerRadius: 16)
+            context.addPath(bezierPath.cgPath)
+
+            context.drawPath(using: .fill)
             
             if drawBorder
             {
