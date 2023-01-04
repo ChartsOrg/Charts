6ebcd6c175eb0ad797a10927e51829926c3374d8
huangchengrui <19801020502@163.com>
2023-01-04 10:20:19 +0800

    fix:解决XCode无法运行的问题

--- Source/Charts/Animation/ChartAnimationEasing.swift
+++ Source/Charts/Animation/ChartAnimationEasing.swift
@@ -341,7 +341,8 @@ internal struct EasingFunctions
         let s: TimeInterval = 1.70158
         var position: TimeInterval = elapsed / duration
         position -= 1.0
-        return Double( position * position * ((s + 1.0) * position + s) + 1.0 )
+        let result = position * position * ((s + 1.0) * position + s)
+        return Double( result + 1.0 )
     }
     
     internal static let EaseInOutBack = { (elapsed: TimeInterval, duration: TimeInterval) -> Double in
